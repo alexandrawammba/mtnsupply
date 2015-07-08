67d4758c30e8287222557931f00e92a7a5276f67
Alexandra_Wamba <alexandrawamba@gmail.com>
2015-07-08 15:38:08 +0100

    Le projet MtnSupply

---
+++ src/com/example/mtnsupply/Chargement.java
@@ -0,0 +1,15 @@
+package com.example.mtnsupply;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Chargement extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.chargement);
+	}
+
+	
+}
